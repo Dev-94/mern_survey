ea0a93696a1c5b5237889e27d8a3747c84501a22
Dev Govindji <friendsofdev94@gmail.com>
2019-11-13 13:49:51 -0600

    installs alternative to google oauth api

--- server/index.js
+++ server/index.js
@@ -8,3 +8,10 @@ passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+
+
+// where login should redirect to:
+    // http://localhost:5000/auth/google/callback
+
+// Google API no longer exists

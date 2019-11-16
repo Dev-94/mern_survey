a50821c0d942071d5c0d90f5bf628c289e140376
Dev Govindji <friendsofdev94@gmail.com>
2019-11-16 16:10:33 -0600

    sets up oauth callback

--- server/index.js
+++ server/index.js
@@ -34,3 +34,13 @@ app.listen(PORT);
     // http://localhost:5000/auth/google/callback
 
 // Google API no longer exists
+
+
+
+//https://accounts.google.com/o/oauth2/v2/auth?
+//response_type=code&
+
+//redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fauth%2Fgoogle%2Fcallback&
+//scope=profile%20email&
+
+//client_id=186756091413-9n4r5nidau5i03hbs08vvh7n5569hr3f.apps.googleusercontent.com
